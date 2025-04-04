826c69d9d6e52176e6b93d335d80a62520695f95
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2025-04-04 13:05:07 -0700

    Remove jQuery and more

--- resources/assets/js/spark-bootstrap.js
+++ resources/assets/js/spark-bootstrap.js
@@ -5,7 +5,6 @@ import lodash from 'lodash';
 import moment from 'moment';
 import Promise from 'promise';
 import Popper from 'popper.js';
-import jQuery from 'jquery';
 import mixin from './mixin.js';
 import Dinero from 'dinero.js';
 import SparkForm from './forms/form.js';
@@ -29,36 +28,6 @@ window.__ = (key, replace) => {
 
     return translation;
 };
-
-/*
- * Define Moment locales
- */
-window.moment.defineLocale('en-short', {
-    parentLocale: 'en',
-    relativeTime : {
-        future: "in %s",
-        past:   "%s",
-        s:  "1s",
-        m:  "1m",
-        mm: "%dm",
-        h:  "1h",
-        hh: "%dh",
-        d:  "1d",
-        dd: "%dd",
-        M:  "1 month ago",
-        MM: "%d months ago",
-        y:  "1y",
-        yy: "%dy"
-    }
-});
-window.moment.locale('en');
-
-/*
- * Load jQuery and Bootstrap jQuery, used for front-end interaction.
- */
-if (window.$ === undefined || window.jQuery === undefined) {
-    window.$ = window.jQuery = jQuery;
-}
 
 /**
  * Load Vue if this application is using Vue as its framework.
@@ -139,39 +108,3 @@ if ($('#spark-app').length > 0) {
      */
     $.extend(Spark, http);
 }
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-window.axios.defaults.headers.common = {
-    'X-Requested-With': 'XMLHttpRequest',
-    'X-CSRF-TOKEN': Spark.csrfToken
-};
-
-/**
- * Intercept the incoming responses.
- *
- * Handle any unexpected HTTP errors and pop up modals, etc.
- */
-window.axios.interceptors.response.use(function (response) {
-    return response;
-}, function (error) {
-    if (error.response === undefined) {
-        return Promise.reject(error);
-    }
-
-    switch (error.response.status) {
-        case 401:
-            window.axios.get('/logout');
-            $('#modal-session-expired').modal('show');
-            break;
-
-        case 402:
-            window.location = '/settings#/subscription';
-            break;
-    }
-
-    return Promise.reject(error);
-});
